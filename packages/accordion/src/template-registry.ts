fd57f99a1e240223fce840a57fad492bd07f004b
Lukas Nys <lukas.nys@otainsight.com>
2023-09-28 15:18:24 +0200

    add components to registry

--- packages/accordion/src/template-registry.ts
+++ packages/accordion/src/template-registry.ts
@@ -3,9 +3,19 @@
 // See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
 
 // import type MyComponent from './components/my-component';
+import type Accordion from './components/accordion';
+import type AccordionItem from './components/accordion/item';
+import type AccordionHeader from './components/accordion/header';
+import type AccordionTrigger from './components/accordion/trigger';
+import type AccordionContent from './components/accordion/content';
 
 // Remove this once entries have been added! 👇
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export default interface Registry {
   // MyComponent: typeof MyComponent
+  Accordion: typeof Accordion;
+  AccordionItem: typeof AccordionItem;
+  AccordionHeader: typeof AccordionHeader;
+  AccordionTrigger: typeof AccordionTrigger;
+  AccordionContent: typeof AccordionContent;
 }
